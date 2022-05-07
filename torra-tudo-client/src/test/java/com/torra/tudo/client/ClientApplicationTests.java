9af999b64392c01d3dd173d68774f112e8341f43
Juan Pablo <juanpablopereiragomes@hotmail.com>
2022-05-07 14:30:23 -0300

    starter gateway and eureka client

--- torra-tudo-client/src/test/java/com/torra/tudo/client/ClientApplicationTests.java
+++ torra-tudo-client/src/test/java/com/torra/tudo/client/ClientApplicationTests.java
@@ -3,11 +3,11 @@ package com.torra.tudo.client;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
-//class ClientMapperApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- torra-tudo-product/src/main/java/com/torra/tudo/product/ProductApplication.java
+++ torra-tudo-product/src/main/java/com/torra/tudo/product/ProductApplication.java
@@ -2,12 +2,14 @@ package com.torra.tudo.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class TorraTudoApplication {
+@EnableEurekaClient
+public class ProductApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TorraTudoApplication.class, args);
+		SpringApplication.run(ProductApplication.class, args);
 	}
 
 }
