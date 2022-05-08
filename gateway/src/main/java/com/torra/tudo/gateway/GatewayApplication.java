d0841e8cb9f2820623e389d07184d832c4a647ba
Juan Pablo <juanpablopereiragomes@hotmail.com>
2022-05-08 15:46:24 -0300

    created routes to service client and product

--- gateway/src/main/java/com/torra/tudo/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/torra/tudo/gateway/GatewayApplication.java
@@ -2,16 +2,24 @@ package com.torra.tudo.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableZuulProxy
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
+//    @Bean
+//    public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route(p -> p.path("/client"))
+//                .build();
+//    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
 
 }
--- torra-tudo-product/src/main/java/com/torra/tudo/product/controller/ProductController.java
+++ torra-tudo-product/src/main/java/com/torra/tudo/product/controller/ProductController.java
@@ -2,9 +2,11 @@ package com.torra.tudo.product.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("product")
+@RestController
+@RequestMapping("product")
 @Log4j2
 public class ProductController {
 
